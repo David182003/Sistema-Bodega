67cfbefb148c9a834d278f99ec3673eb3004feb7
David182003 <jorgeminano18@gmail.com>
2025-01-19 08:03:35 -0500

    V-5.0.1

--- script.js
+++ script.js
@@ -5,8 +5,17 @@ document.addEventListener('DOMContentLoaded', () => {
     saldo.addEventListener('click', () => {
         if (saldoNum.style.display === 'none') {
             saldoNum.style.display = 'block';
+            saldoNum.style.display = 'flex';
+            saldoNum.style.alignItems = 'center';
         } else {
             saldoNum.style.display = 'none';
         }
+    });
+
+    document.getElementById('exportarPDF').addEventListener('click', () => {
+        const { jsPDF } = window.jspdf;
+        const doc = new jsPDF();
+        doc.autoTable({ html: '.movimientos_table' });
+        doc.save('movimientos.pdf');
     });
 });
